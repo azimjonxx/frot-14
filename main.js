07c199442039e3b902d0efe43b015bea94917077
azimjonxatamov <azimjonxatamov6@gmail.com>
2024-06-25 17:20:22 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+let ism = prompt ('Ismingizni kiriting  ' )
+let familiya = prompt ('familangizni kiriting')
+let yosh = prompt ('Yoshingizni kiriting')
+let fan = prompt ('Yoqtirgan faningizni yozing')
+let tuman = prompt ('Yashyotgan tumaningizni yozing')
+
+
+alert('Malumatlaringiz kiritkani uchun tashakur')
+console.log('ism : ' + ism ) 
+console.log('familiya:' + familiya )
+console.log('yosh :' + yosh)
+console.log('yoqtirgan fan:' + fan)
+console.log('yashash manzil:' + tuman)
+
+
+
+
+
+
+
+
+
